78ca5f8397683f02b4678bbbc74dcfa00d7c148b
Hamid Gasmi <mamid1706@hotmail.fr>
2020-11-05 15:20:06 -0800

    Add script for terraform
     - This script will contain all commands related to terraform
    - For now, it is a draft

---
+++ terraform.sh
@@ -0,0 +1,5 @@
+terraform init #within /terraform folder first
+terraform -plan #to see changes
+terraform fmt #to format your terraform scripts
+terraform apply #to create
+terraform remove #to destroy
