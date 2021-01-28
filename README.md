47fdbe2fdfdbdb2d6de3fd4b5779374bcaf609f0
Hamid Gasmi <mamid1706@hotmail.fr>
2021-01-28 08:59:37 -0800

    Add docker commands with networking

--- README.md
+++ README.md
@@ -55,14 +55,57 @@
     - The container has a name. 
     - Docker will make a name up if it isnot included in the command
     - Exit: `CTL d` or `exit`
+    - Resource Constraints:
+        - Memory limits: docker run --memory maximum-allowed-memory {image-name} {command}
 - docker attach:
     - Detach and keep a docker running: `ctl+p ctl+q`
 - docker exec:
     - Starts another process in an existing container
     - Great for debugging and DB administration
     - Can't add ports, volumes, etc.
-    
-    
+- docker logs:
+    - To look at container Output
+    - Good particularly when a container stopped
+    - Logs are available as long as their container is also available
+    - Don't let the output get too large: it could slow down Docker to the point where our whole system becomes unresponsive
+- docker kill
+- docker rm
+
+</details>
+
+<details>
+<summary>Anti-pattern and Best practices</summary>
+
+- Don't let our containers fetch dependencies when they start:
+    - Dependencies may be unavailable when container starts.
+    - Include all dependencies in the container image.
+    - E.g. An docker image requires node.js libraries but it doesn't include them:
+        - When a container is created from this image, it fetches all node.js libraries that it needs
+        - Some day, a library could be removed from node.js repo, all our containers just stops
+- Don't leave important things in unnamed stopped containers:
+    - You may remove them when you need to clean up stopped containers
+
+</details>
+
+<details>
+<summary>Networking</summary>
+
+- Programs in containers are isolated from the internet by default
+- We can group our containers into private networks
+- We explicitly choose who can connect to whom
+- Dockers let Expose/Publish ports:
+    - It makes the port accessible from outside the machine on which the docker is beeing hosted
+    - It let connections in
+    - It let private networks to connect between containers
+    - To do it we need to specify: 
+        - The **internal port** our program is listening on
+        - The **external port** our container should be listening on the outside
+        - The protocole to use
+    - Expose ports Dynamically:
+        - The port inside the container is fixed
+        - The port on the host is chosen from the unused ports
+        - This allows many containers running programs with fixed ports
+        - This often is used with a service discovery program like Kubernetees
 
 </details>
 
--- docker_getstarted.sh
+++ docker_getstarted.sh
@@ -39,7 +39,10 @@ docker run -ti ubuntu             # latest is the default tag
 ## DISTRIB_DESCRIPTION="Ubuntu 20.04.1 LTS"
 ## exit (or CTL d)
 # 2.2. ... by using image ID
+docker run -ti {image-ID} bash
 docker run -ti bf756fb1ae65 bash
+# 2.3. ... + give the container a name
+docker run --name hamid-container -ti ubuntu bash
 # 2.3. ... + add command
 docker run -ti ubuntu bash -c "sleep 3; echo all done"
 # 2.4. ... for N seconds
@@ -48,7 +51,52 @@ docker run -ti ubuntu bash -c "sleep 3; echo all done" sleep 5
 docker run --rm -ti ubuntu bash -c "sleep 3; echo all done" sleep 5
 # 2.5. ... detached
 docker run -d -ti ubuntu bash
-# 2.10. Running containers
+# 2.10. Resource Constraints:
+# 2.11. Memory Limits
+docker run --memory maximum-allowed-memory {image-name} {command}
+# 2.12. CPU Limits:
+docker run --cpu-shares {relative to other containers} {image-name} {command} # Relative limit
+docker run --cpu-quota {hard limit} # Hard limit
+# 2.20 Networking: -p: Publish a port
+docker run -p outside-port-to-publish:inside-port-to-listen-to/protocol #(tcp/udp)
+# ... exposing/publishing 2 ports:
+docker run --rm -ti -p 45678:45678 -p 45679:45679 --name echo-server ubuntu:14.04 bash
+nc -lp 45678 | nc -lp 45679
+# ... In a 2nd terminal:
+docker run --rm -ti ubuntu:14.04 bash
+> nc host.docker.internal 45678
+# ... In a 3rd terminal:
+docker run --rm -ti ubuntu:14.04 bash
+> nc host.docker.internal 45679
+# ... from the 2nd terminal
+Test message
+# ... In the 3rd terminal
+Test message
+# ... 2nd ncat -> 1st. ncat -> 3rd. ncat
+# 2.21 Networking: -p: Publish ports dynamically
+docker run --rm -ti -p 45678 -p 45679 --name echo-server ubuntu:14.04 bash
+nc -lp 45678 | nc -lp 45679
+# ... in another terminal:
+docker port echo-server
+45678/tcp -> 0.0.0.0:49154
+45679/tcp -> 0.0.0.0:49274
+# ... In a 2nd terminal:
+docker run --rm -ti ubuntu:14.04 bash
+> nc host.docker.internal 49154 # outside port
+# ... In a 3nd terminal:
+docker run --rm -ti ubuntu:14.04 bash
+> nc host.docker.internal 49274 # outside port
+# 2.20 Networking: -p: UDP port
+docker run --rm -ti -p 45678/udp --name echo-server ubuntu:14.04 bash
+> nc -ulp 45678
+# ... in another terminal:
+docker port echo-server
+45678/tcp -> 0.0.0.0:49154
+# ... In a 2nd terminal:
+docker run --rm -ti ubuntu:14.04 bash
+> nc -u host.docker.internal 49154 # outside port -u for udp
+
+# 3. docker ps: Running containers
 docker ps   # with default formatting (horizontal)
 docker ps --format "\nID\t{{.ID}}\nIMAGE\t{{.Image}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.RunningFor}}\nSTATUS\t{{.Status}}\nPORTS\t{{.Ports}}\nNAMES\t{{.Names}}\n"
 docker ps -a # to see all container, even those that are stopped
@@ -58,13 +106,9 @@ Status 0   : succes
 Status 127 :
 Status 130 :
 ## Check PORT for networking
-# 2.20. attach a detached container
+# 2.30. attach a detached container
 docker attach {container-name}
 #... Detach and keep a docker running: ctl+p ctl+q
-# 2.30. remove a container
-docker rm {container-ID}
-# 2.31. remove all containers
-docker rm $(docker ps -a -q)
 
 # 3. Create an docker image out of an docker container
 # 3.1. Commit + Tag commands
@@ -76,9 +120,22 @@ docker commit {container-name} {image-name}
 docker images # to find the new image
 
 # 4. Exec new process
-docker exec [-flags][container-name]
+docker exec [-flags] {container-name}
 docker exec -ti happy_kare bash
 
+# 5. Logs
+docker logs {container-ID}
+docker logs {container-name}
+
+# 6. Stop a container
+docker kill {container-ID}
+docker kill {container-name}
+
+# 7. remove a container
+docker rm {container-ID}
+docker rm {container-name}
+# ... remove all containers
+docker rm $(docker ps -a -q)
 
 
 
